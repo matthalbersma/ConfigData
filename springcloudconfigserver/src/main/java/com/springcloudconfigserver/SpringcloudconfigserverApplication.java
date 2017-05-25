d2806780756d45fe042a52fcfea8c4c22f1b06ef
Matthew Halbersma <mhalbers@nerdery.com>
2017-05-25 13:38:54 -0500

    new config server

---
+++ springcloudconfigserver/src/main/java/com/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigserverApplication.class, args);
+	}
+}
